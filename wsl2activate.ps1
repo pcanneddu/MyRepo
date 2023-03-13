9a4c4d6faa48c7d5e5c32441d0f044a86c391af9
pcanneddu <85615111+pcanneddu@users.noreply.github.com>
2023-03-13 17:13:08 +0100

    Update wsl2activate.ps1

--- wsl2activate.ps1
+++ wsl2activate.ps1
@@ -2,3 +2,5 @@
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart 
 # Enable the Virtual Machine Platform 
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+# Install WSL2
+wsl --install
