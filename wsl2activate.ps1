a12d573e9eb86bb7a1701ed3db188ae88bf4eb41
pcanneddu <85615111+pcanneddu@users.noreply.github.com>
2023-03-13 11:23:10 +0100

    Create wsl2activate.ps1

---
+++ wsl2activate.ps1
@@ -0,0 +1,4 @@
+# Enable the WSL2 without reboot 
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart 
+# Enable the Virtual Machine Platform 
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
